39b21e31a0dd326bb62df8e6f81cb7ad53b03f67
Dominique Skaf <doskaf@yahoo.com>
2020-03-02 16:23:15 -0600

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,32 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let modal = document.getElementById('modal');
+modal.className = "hidden";
 
-
-
+document.querySelectorAll('.like').forEach(likeBtn => {
+  likeBtn.addEventListener("click", function(){
+    mimicServerCall()
+    .then(function(response) {
+      return response;
+    })
+    .then(function(json) {
+      let heart = likeBtn.children[0];
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else if (heart.innerText === FULL_HEART) {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      modal.innerText = `ERROR: ${error}`;
+      modal.className = "";
+      setTimeout(function(){ modal.className = "hidden" }, 5000);
+    });
+  });
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
